560f3a4730b9ed45d4d31d57b9f9797b5d7c223e
kcherm <kevinh2208@outlook.comgit>
2021-09-24 17:11:06 -0400

    Additional Improvements

--- app/src/main/java/com/example/loancalc/MainActivity.java
+++ app/src/main/java/com/example/loancalc/MainActivity.java
@@ -12,18 +12,24 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
 
     public Button button;
+    public EditText editPurchase;
+    public EditText editDown;
+    public EditText editIntRate;
+    public EditText editLength;
+    public TextView textOutput;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editPurchase = findViewById(R.id.editPurchase);
-        EditText editDown = findViewById(R.id.editDown);
-        EditText editIntRate = findViewById(R.id.editIntRate);
-        EditText editLength = findViewById(R.id.editLength);
-        Button button = findViewById(R.id.button);
-        TextView textOutput = findViewById(R.id.textOutput);
+        editPurchase = findViewById(R.id.editPurchase);
+        editDown = findViewById(R.id.editDown);
+        editIntRate = findViewById(R.id.editIntRate);
+        editLength = findViewById(R.id.editLength);
+        button = findViewById(R.id.button);
+        textOutput = findViewById(R.id.textOutput);
 
         initToggleButton();
         setForEditing(true);
@@ -32,23 +38,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 double price = Double.parseDouble(editPurchase.getText().toString());
-                double down = Double.parseDouble(editDown.getText().toString());
-                double rate = Double.parseDouble(editIntRate.getText().toString());
                 double len = Double.parseDouble(editLength.getText().toString());
-
-                double dec = rate/100;
-                double mr = dec/12;
-                double pow = Math.pow(1+mr,-len);
-
-                double total = price-down;
-                double payment = mr * total / (1-(pow));
-
-
-                textOutput.setText("The payment is $" +String.format("%.2f",payment)+"/month.");
+                calculate(len,price);
             }
         });
     }
 
+    public void calculate(double len, double price ) {
+        double down = Double.parseDouble(editDown.getText().toString());
+        double rate = Double.parseDouble(editIntRate.getText().toString());
+
+        double dec = rate/100;
+        double mr = dec/12;
+        double pow = Math.pow(1+mr,-len);
+
+        double total = price-down;
+        double payment = mr * total / (1-(pow));
+
+
+
+        textOutput.setText("The payment is $" +String.format("%.2f",payment)+"/month.");
+    }
 
     public void initToggleButton(){
         ToggleButton toggleButton = findViewById(R.id.toggleButton);
@@ -62,7 +72,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setForEditing(boolean enabled){
-          EditText editLength = findViewById(R.id.editLength);
+          editLength = findViewById(R.id.editLength);
+//          editPurchase = findViewById(R.id.editPurchase);
+//          double len = Double.parseDouble(editLength.getText().toString());
+//          double price = Double.parseDouble(editPurchase.getText().toString());
           editLength.setEnabled(enabled);
+
+//          calculate(len,price);
     }
 }
